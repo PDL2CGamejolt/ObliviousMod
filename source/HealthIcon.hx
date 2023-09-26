be7d4d84038488e29717dbdc0461d6ace05a3fbb
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2023-09-26 07:20:29 -0500

    Code from old Payten Edition part 1

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -1,6 +1,5 @@
 package;
 
-import flixel.math.FlxMath;
 import flixel.FlxSprite;
 import openfl.utils.Assets as OpenFlAssets;
 
@@ -9,12 +8,11 @@ using StringTools;
 class HealthIcon extends FlxSprite
 {
 	public var sprTracker:FlxSprite;
-	public var canBounce:Bool = false;
 	private var isOldIcon:Bool = false;
 	private var isPlayer:Bool = false;
 	private var char:String = '';
 
-	public function new(char:String = 'bf', isPlayer:Bool = false, ?allowGPU:Bool = true)
+	public function new(char:String = 'bf', isPlayer:Bool = false)
 	{
 		super();
 		isOldIcon = (char == 'bf-old');
@@ -29,20 +27,22 @@ class HealthIcon extends FlxSprite
 
 		if (sprTracker != null)
 			setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 30);
-
-		if(canBounce) {
-			var mult:Float = FlxMath.lerp(1, scale.x, CoolUtil.boundTo(1 - (elapsed * 9), 0, 1));
-			scale.set(mult, mult);
-			updateHitbox();
-		}
 	}
 
 	public function swapOldIcon() {
-		if(isOldIcon = !isOldIcon) changeIcon('bf-old');
-		else changeIcon('bf');
+		changeIcon(if(isOldIcon = !isOldIcon) 'bf-old' else 'bf');
 	}
 
-	private var iconOffsets:Array<Float> = [0, 0, 0];
+	private var iconOffsets:Array<Float> = [0, 0];
+
+
+	public dynamic function updateAnim(health:Float){ // Dynamic to prevent having like 20 if statements
+		if (health < 20)
+			animation.curAnim.curFrame = 1;
+		else
+			animation.curAnim.curFrame = 0;
+	}
+
 	public function changeIcon(char:String) {
 		if(this.char != char) {
 			var name:String = 'icons/' + char;
@@ -50,25 +50,35 @@ class HealthIcon extends FlxSprite
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
 			var file:Dynamic = Paths.image(name);
 
-			loadGraphic(file); //Load stupidly first for getting the file size
-			var width2 = width;
-			if (width == 450) {
-				loadGraphic(file, true, Math.floor(width / 3), Math.floor(height)); //Then load it fr // winning icons go br
-				iconOffsets[0] = (width - 150) / 3;
-				iconOffsets[1] = (width - 150) / 3;
-				iconOffsets[2] = (width - 150) / 3;
-			} else {
-				loadGraphic(file, true, Math.floor(width / 2), Math.floor(height)); //Then load it fr // winning icons go br
-				iconOffsets[0] = (width - 150) / 2;
-				iconOffsets[1] = (width - 150) / 2;
-			}
 			
-			updateHitbox();
-			if (width2 == 450) {
-				animation.add(char, [0, 1, 2], 0, false, isPlayer);
-			} else {
-				animation.add(char, [0, 1], 0, false, isPlayer);
+			var icoheight:Int = 150;
+			var icowidth:Int = 150;
+			var frameCount = 1; // Has to be 1 instead of 2 due to how compooters handle numbers
+			if(width % 150 != 0 || height % 150 != 0){ // Invalid sized health icon! Split in half
+				icoheight = file.height;
+				icowidth = Std.int(file.width * 0.5);
+			}else{
+				frameCount = Std.int(file.width / 150) - 1; // If this isn't an integer, fucking run
+				if(frameCount == 0) updateAnim = function(health:Float){return;};
+				else if(frameCount == 1) updateAnim = function(health:Float){
+					if (health < 20)
+						animation.curAnim.curFrame = 1;
+					else
+						animation.curAnim.curFrame = 0;
+				};
+				// This goes from Losing to Winning
+				else if(frameCount > 1) updateAnim = function(health:Float){animation.curAnim.curFrame = Math.round( animation.curAnim.numFrames * (health / 100) );};
+				
 			}
+			loadGraphic(file, true, icowidth, icoheight); //Then load it fr
+			iconOffsets[0] = (width - 150) / 2;
+			iconOffsets[1] = (width - 150) / 2;
+			updateHitbox();
+
+
+			frameCount = frameCount + 1;
+			animation.add(char, if(frameCount > 1)[for (i in 0 ... frameCount) i] else [0,1], 0, false, isPlayer);
+
 			animation.play(char);
 			this.char = char;
 
@@ -79,12 +89,11 @@ class HealthIcon extends FlxSprite
 		}
 	}
 
-	public function bounce() {
-		if(canBounce) {
-			var mult:Float = 1.2;
-			scale.set(mult, mult);
-			updateHitbox();
-		}
+	override function updateHitbox()
+	{
+		super.updateHitbox();
+		offset.x = iconOffsets[0];
+		offset.y = iconOffsets[1];
 	}
 
 	public function getCharacter():String {
