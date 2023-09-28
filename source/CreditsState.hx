4810db4fa48d6963ccb7939497785c2998dbf2b3
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2023-09-28 15:53:48 -0500

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -112,7 +112,9 @@ class CreditsState extends MusicBeatState
 			['ninjamuffin99',		'ninjamuffin99',	"Programmer of Friday Night Funkin'",							'https://twitter.com/ninja_muffin99',	'CF2D2D'],
 			['PhantomArcade',		'phantomarcade',	"Animator of Friday Night Funkin'",								'https://twitter.com/PhantomArcade3K',	'FADC45'],
 			['evilsk8r',			'evilsk8r',			"Artist of Friday Night Funkin'",								'https://twitter.com/evilsk8r',			'5ABD4B'],
-			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",								'https://twitter.com/kawaisprite',		'378FC7']
+			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",								'https://twitter.com/kawaisprite',		'378FC7'],
+			['Others'],
+			['Everyone Else',			'copyright',				'The things I did not make.',								'https://en.wikipedia.org/wiki/Copyright',			'FFFFFF']
 		];
 		
 		for(i in pisspoop){
