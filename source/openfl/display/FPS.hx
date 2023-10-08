3127eb2fdd0716ca347a05cd6d510c0648716699
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2023-10-08 09:46:36 -0500

    Update FPS.hx

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -82,7 +82,6 @@ class FPS extends TextField
 
 		if (currentCount != cacheCount /*&& visible*/)
 		{
-                        textColor = 0xFFFFFFFF;
 			text = "Frames Per Second: " + currentFPS;
 		        text += "\nMemory: " + CoolUtil.formatBytes(Memory.getCurrentUsage()) + (ClientPrefs.showMaxRamUsage ? " / " + CoolUtil.formatBytes(Memory.getPeakUsage()) : "");
 			text += "\nVanta Engine";
@@ -90,6 +89,14 @@ class FPS extends TextField
 			text += "\nA modified version of JSE v1.10.0";
 			text += "\n";
 			#end
+		}
+
+		textColor = 0xFFFFFFFF;
+		
+		if (currentFPS <= ClientPrefs.framerate / 2)
+		{
+			textColor = 0xFFFF0000;
+		}
 		}
 
 		cacheCount = currentCount;
