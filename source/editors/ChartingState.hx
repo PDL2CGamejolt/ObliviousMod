c92adeb5fdb713744b8d66b1f3d96cad4677e45c
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2023-10-20 16:55:26 -0500

    Update ChartingState.hx

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -1887,10 +1887,9 @@ class ChartingState extends MusicBeatState
 		}
 		}
 
-{
+{}
                 FlxG.watch.addQuick('daBeat', curBeat);
                 FlxG.watch.addQuick('daStep', curStep);
-}
 					
 		if (FlxG.mouse.x > gridBG.x
 			&& FlxG.mouse.x < gridBG.x + gridBG.width
