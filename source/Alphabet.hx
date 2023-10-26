48c63e7f1965b22a90ced30330fa6e5677a0f2ba
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2023-10-25 20:07:00 -0500

    Update Alphabet.hx

--- source/Alphabet.hx
+++ source/Alphabet.hx
@@ -165,6 +165,7 @@ class Alphabet extends FlxSpriteGroup
 				x = FlxMath.lerp(x, (targetY * distancePerItem.x) + startPosition.x, lerpVal);
 			if(changeY)
 				y = FlxMath.lerp(y, (targetY * 1.3 * distancePerItem.y) + startPosition.y, lerpVal);
+                        screenCenter(X);
 		}
 		super.update(elapsed);
 	}
